752e3a3ce5673ddcf7468330d97fae4fd2800207
jesusmmen28 <jesusmmen28@gmail.com>
2023-02-06 14:32:08 +0100

    cambio de endpoint link

--- server.js
+++ server.js
@@ -44,8 +44,8 @@ app.put('/user/:id', authUser, editUser); //modificar usuario
 //Rutas de link
 app.post('/', authUser, newLinkController); //creo los link
 app.get('/', authUser, getLinksController); //listo los link, incluyendo los votos que tienen cada uno
-app.get('/link/:id', authUser, getSingleLinkController, ); //Devuelvo un link
-app.delete('/link/:id', authUser, deleteLinkController); //borro un link
+app.get('/enlace/:id', authUser, getSingleLinkController, ); //Devuelvo un link
+app.delete('/enlace/:id', authUser, deleteLinkController); //borro un link
 
 //ruta de votos
 app.post('/votes/:id', authUser, votesController);
